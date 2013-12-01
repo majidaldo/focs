b5eca6074fc0816c98c245ca59c45c348c676f4a
majidaldo <majidaldosar-github@yahoo.com>
2013-11-30 21:22:56 -0500

    before making dot prod of rhs delNi*phi

--- fem/fem.c
+++ fem/fem.c
@@ -4,6 +4,13 @@
 #include <stdlib.h>
 #include <math.h>
 
+/*
+?:
+-dot prod rhs?
+-0s for missing phis?
+*/
+
+
 typedef float mf;//myfloat
 typedef unsigned int mui;
 
@@ -17,17 +24,17 @@ typedef struct {
   void *data;
   mui nrows,ncols; 
   typeenum typenum;
-} matrixstruct;
+} structmatrix;
 
-matrixstruct makematrix(mui nrows, mui ncols, typeenum tn){
-  matrixstruct mat;
+structmatrix makematrix(mui nrows, mui ncols, typeenum tn){
+  structmatrix mat;
   mat.nrows=nrows;
   mat.ncols=ncols;
   mat.typenum=tn;
   mat.data=malloc(nrows*ncols*typesizes[tn]);//don't forget to call free()
   return mat; 
 }
-void *idx(mui *ri, mui *ci, matrixstruct *amat){
+void *idx(mui *ri, mui *ci, structmatrix *amat){
   if((*ri>=amat->nrows) || (*ci>=amat->ncols)){ printf("outofbounds %u %u",*ri,*ci);exit(1);}
   //col mjr
 #define poffset typesizes[amat->typenum]*((*ri)+(amat->nrows)*(*ci))
@@ -35,7 +42,7 @@ void *idx(mui *ri, mui *ci, matrixstruct *amat){
 #undef poffset
 }
 
-void initfltmatrix(mf iv, matrixstruct *m){
+void initfltmatrix(mf iv, structmatrix *m){//could just do memset
   mui ri,ci;
   mf *v;
   for(ci=0;ci<m->ncols;ci++){
@@ -45,8 +52,8 @@ void initfltmatrix(mf iv, matrixstruct *m){
 }
 
 typedef struct {
-  matrixstruct coords;
-  matrixstruct conn;
+  structmatrix coords;
+  structmatrix conn;
 } gridstruct ;
 
 mui MAXLINE=50;
@@ -59,14 +66,14 @@ FILE *readto(FILE *fp, char *line){
   return fp;
 }
 
-matrixstruct readcoords(FILE *fp){
+structmatrix readcoords(FILE *fp){
   mui npts;
   char ptsstr[]="POINTS";
   fp=readto(fp,ptsstr);
   fscanf(fp,"%u",&npts);
 
-  matrixstruct m=makematrix(npts,2,floatt);
-  mui ri,xc=0,yc=1;mf dontcare;//not storing 0 (3rd num)
+  structmatrix m=makematrix(npts,2,floatt);
+  mui ri,xc=0,yc=1;mf dontcare;//not storing 0s (3rd dim)
   for(ri=0;ri<npts;ri++){
     fscanf(fp, "%f %f %f",(mf*)idx(&ri,&xc,&m)
 	                 ,(mf*)idx(&ri,&yc,&m),&dontcare);
@@ -74,13 +81,13 @@ matrixstruct readcoords(FILE *fp){
 
   return m;
 }
-matrixstruct readconn(FILE *fp){
+structmatrix readconn(FILE *fp){
   mui ntri;
   char meshstr[]="mesh";
   fp=readto(fp,meshstr);
   fscanf(fp,"%u",&ntri);
 
-  matrixstruct m=makematrix(ntri,3,uintt);
+  structmatrix m=makematrix(ntri,3,uintt);
   mui ri,ac=0,bc=1,cc=2;
   mui a,b,c,*pa,*pb,*pc;
   for(ri=0;ri<ntri;ri++){
@@ -95,8 +102,8 @@ matrixstruct readconn(FILE *fp){
 }
 
 
-matrixstruct readphi(FILE *fp, mui nphi){
-  matrixstruct m=makematrix(nphi,1,floatt);
+structmatrix readphi(FILE *fp, mui nphi){
+  structmatrix m=makematrix(nphi,1,floatt);
 
   mui ri=0,c=0;
   for (ri=0;ri<nphi;ri++){
@@ -110,8 +117,8 @@ matrixstruct readphi(FILE *fp, mui nphi){
 
 
 
-typedef struct { matrixstruct D; mf A;} dercalcout;
-dercalcout calcdernA(mui *ti,matrixstruct *conn, matrixstruct *coords){
+typedef struct { structmatrix D; mf A;} dercalcout;
+dercalcout calcdernA(mui *ti,structmatrix *conn, structmatrix *coords){
   //gives the derivative of element div by 2A
   //mui ti//,ci;//ti for triangle index
   mui ac=0,bc=1,cc=2,xc=0,yc=1;
@@ -122,7 +129,7 @@ dercalcout calcdernA(mui *ti,matrixstruct *conn, matrixstruct *coords){
                       +(*(mf*)idx(&bc,&xcyc,coords))	\
                       -(*(mf*)idx(&a ,&xcyc,coords))
 
-  matrixstruct D=makematrix(conn->ncols,2,floatt);//acol for x,y
+  structmatrix D=makematrix(conn->ncols,2,floatt);//acol for x,y
   //for(ti=0;ti<A.nrows;ti++){
     a=(*(mui*)idx(ti,&ac,conn));
     b=(*(mui*)idx(ti,&bc,conn));
@@ -149,12 +156,81 @@ dercalcout calcdernA(mui *ti,matrixstruct *conn, matrixstruct *coords){
 }
 
 
- /* parea=(mf*)idx(&seven,&zero,&A); */
- /*    *parea=.5*(abx*acy-aby*acx) */
 
 
+structmatrix matrixmul(structmatrix *A,structmatrix *B){
+  if(A->ncols!=B->nrows)
+    {printf("incompatible matrices for multiplication");exit(1);}
 
-void printmat(matrixstruct *mat){
+  structmatrix C=makematrix(A->nrows,B->ncols,floatt);
+#define  Bv(ri,ci) *(mf*) idx(&ri,&ci, B)
+#define  Av(ri,ci) *(mf*) idx(&ri,&ci, A)
+#define  Cv(ri,ci) *(mf*) idx(&ri,&ci,&C)
+
+  mui ri,ci,ici;
+  for(ri=0;ri<C.nrows;ri++){//for a row,col in product, it's the summation of
+    for(ci=0;ci<C.ncols;ci++){//...row A times col B
+
+      mf sum=0;
+      for(ici=0;ici<A->ncols;ici++){
+	//double a=Av(ri,ici),b=Bv(ci,ici);
+	;sum+=(Av(ri,ici)*Bv(ici,ci));}
+
+      Cv(ri,ci)=sum;
+    }
+  }
+
+  return C;
+
+#undef Av
+#undef Bv
+#undef Cv
+}
+
+
+structmatrix calcd( structmatrix *Mc
+		    ,structmatrix *Ml
+		    ,structmatrix *u){
+
+  structmatrix Mccpy=makematrix(Mc->nrows,Mc->ncols,floatt);
+  memcpy(Mccpy.data,(Mc->data),sizeof(floatt)*(Mc->nrows*Mc->ncols)); 
+  structmatrix d=makematrix(Mc->nrows,1,floatt);
+
+  //Mc - Ml
+  mui ri,zero=0;
+  mf *mcv;
+  for(ri=0;ri<Mc->nrows;ri++){
+             mcv=idx(&ri,&ri  ,&Mccpy);
+    *mcv=  *(mf*)idx(&ri,&ri  ,Mc)
+         -(*(mf*)idx(&ri,&zero,Ml));
+  }
+
+  d=matrixmul(&Mccpy,u);
+  free(Mccpy.data);
+
+  return d;	   
+}
+structmatrix calcup1( structmatrix *r
+		      ,structmatrix *d
+		      ,structmatrix *Mlm1){
+
+  structmatrix up1=makematrix(r->nrows,1,floatt);
+
+
+  mui ri,zero=0;//(r+d)*Ml^-1
+  mf *pup1;
+  for(ri=0;ri<r->nrows;ri++){
+    pup1=  idx(&ri,&zero,&up1);
+    *pup1=(  *(mf*)idx(&ri,&zero,r)
+	    +*(mf*)idx(&ri,&zero,d) )
+           *(*(mf*)idx(&ri,&zero,Mlm1));
+  }
+
+  return up1;
+}
+
+
+void printmat(structmatrix *mat){
   mui ri,ci;
   for(ri=0;ri<mat->nrows;ri++){
     for(ci=0;ci<mat->ncols;ci++){
@@ -175,42 +251,49 @@ int main(){
   FILE *phixfp=fopen( "phi0.0","r");
   FILE *phiyfp=fopen( "phi0.1","r");
 
-  //reading
-  matrixstruct coords=readcoords(gfp);
-  matrixstruct conn=    readconn(gfp);
-  matrixstruct phix=     readphi(phixfp,coords.nrows);
-  matrixstruct phiy=     readphi(phiyfp,coords.nrows);
+  //READING
+  structmatrix coords=readcoords(gfp);
+  structmatrix conn=    readconn(gfp);
+  structmatrix phix=     readphi(phixfp,coords.nrows);
+  structmatrix phiy=     readphi(phiyfp,coords.nrows);
 
-  //global matrix
-  matrixstruct  M=makematrix(coords.nrows,coords.nrows,floatt);
-  matrixstruct rx=makematrix(coords.nrows,1           ,floatt);
-  matrixstruct ry=makematrix(coords.nrows,1           ,floatt);
-  initfltmatrix(0,&M);//set 0s
+  //ALLOC GLOBAL MATRICES
+  structmatrix Mc=makematrix(coords.nrows,coords.nrows,floatt);
+  structmatrix Ml=makematrix(coords.nrows,1           ,floatt);
+  structmatrix rx=makematrix(coords.nrows,1           ,floatt);
+  structmatrix ry=makematrix(coords.nrows,1           ,floatt);
+  initfltmatrix(0,&Mc);//set 0s
+  initfltmatrix(0,&Ml);
   initfltmatrix(0,&rx);
   initfltmatrix(0,&ry);
 
-  //local matrix
+  //LOCAL MATRICES
   mf aNi[3][1]={ {1}
 		,{1}
 		,{1}};//  *A/3
-  matrixstruct INi=makematrix(3,1,floatt);
+  structmatrix INi   =makematrix(3,1,floatt);
   mf aNiNj[3][3]={ {2,1,1}
 		  ,{1,2,1}
 		  ,{1,1,2}}; // *A/12
-  matrixstruct INiNj=makematrix(3,3,floatt);
+  structmatrix INiNj =makematrix(3,3,floatt);
+  /* mf aNiNjl[3][3]={ {4,0,0}// not used */
+  /* 		   ,{0,4,0} */
+  /* 		    ,{0,0,4}};//   *A/12 */
+  /* structmatrix INiNjl=makematrix(3,3,floatt); */
   mui ri,ci;mf *v;
   for(ri=0;ri<3;ri++){
   for(ci=0;ci<1;ci++){ v=(mf*)idx(&ri,&ci,&INi)   ; *v=  aNi[ri][ci];}}
   for(ri=0;ri<3;ri++){
   for(ci=0;ci<3;ci++){ v=(mf*)idx(&ri,&ci,&INiNj) ; *v=aNiNj[ri][ci];}}
+  /* for(ri=0;ri<3;ri++){ */
+  /* for(ci=0;ci<3;ci++){ v=(mf*)idx(&ri,&ci,&INiNjl) ;*v=aNiNjl[ri][ci];}} */
 
 
-
-  //create global matrix 
+  //FILL GLOBAL MATRICES
   mui cri,xc=0,yc=1,zero=0;
   mui sri,sci;
   mui abcr,abcc;
-  mf *src,*Mdst,*rxdst,*rydst;
+  mf *src,*Mcdst,*Mldst,*rxdst,*rydst;
   mf *pphix,*pphiy;
   mf *pDx,*pDy,*pA;
   dercalcout AnD;
@@ -226,11 +309,13 @@ int main(){
       src=          idx(&sri, &sci ,&INiNj);//a value inside the submat
       abcc=*(mui*)  idx(&cri, &sci ,&conn);//the col index that gets a,b,or c
       abcr=*(mui*)  idx(&cri, &sri ,&conn);
-      Mdst=         idx(&abcr,&abcc,&M);// ??? is this ok?
-      *Mdst+=(*src)*(AnD.A)/12.;
+      Mcdst=        idx(&abcr,&abcc,&Mc);// ??? is this ok?
+      *Mcdst+=(*src)*(*pA)/12.;
       //printf("\n %f %u %u",*dst,abcr,abcc);
 	}
+
     abcr=*(mui*)  idx(&cri  ,&sri  ,&conn);//the col index that get a,b, or c
+    Mldst=        idx(&abcr ,&zero ,&Ml);
     src=          idx(&sri  ,&zero ,&INi); //value inside the submat
     rxdst=        idx(&abcr ,&zero ,&rx);  //ptr to destination
     rydst=        idx(&abcr ,&zero ,&ry);  //
@@ -240,24 +325,64 @@ int main(){
     pDy=          idx(&sri  ,&yc   ,&AnD.D);
     *rxdst+=(*src)*(*pA/3.) * (*pDx/(2*(*pA)))*(*pphix);//could elim A here
     *rydst+=(*src)*(*pA/3.) * (*pDy/(2*(*pA)))*(*pphiy);
+    *Mldst+=4.0*(*pA/12);
 }
   free(AnD.D.data);
 
 
-}
+  }//conn row iter
 
 
+  //SOLVING PROCEDURE
+  //make inverse matrix
+  structmatrix Mlm1=makematrix(Ml.nrows,Ml.ncols,floatt);
+  mf *inv;
+  for(ri=0;ri<Mlm1.nrows;ri++){
+    inv=          idx(&ri,&zero,&Mlm1);
+   *inv=pow(*(mf*)idx(&ri,&zero,&Ml),-1);
+  }
 
-  //printmat(&M);
+  structmatrix u  =makematrix(coords.nrows,1,floatt);
+  structmatrix up1=makematrix(coords.nrows,1,floatt); 
+  structmatrix d;
+  initfltmatrix(0,&u);
 
-  //freealloc
+  //iterloop
+  mui i,equals,ii;
+  for(i=0;i<100;i++){
+    d=calcd(&Mc,&Ml,&u);
+    up1=calcup1(&rx,&d,&Mlm1);
+    free(d.data);
+    equals=0;  
+    for(ii=0;ii<up1.nrows;ii++){
+      if( fabs(   (   *(mf*)idx(&ii,&zero,&up1)
+		      -*(mf*)idx(&ii,&zero,&u)
+		      )				\
+		  /  (*(mf*)idx(&ii,&zero,&u))
+		  ) < .01)
+	{equals++;}
+    }
+    free(u.data);
+    u=up1;
+    printf("\n eq %u",equals);
+    if(equals==up1.nrows){break;}
+  }
+
+  // printmat(&u);
   
-  mui tr=911,tc=912;printf("%f",*(float*) idx(&tr,&tc,&M));
 
+   //mui tr=911,tc=912;printf("%f",*(float*) idx(&tr,&tc,&Mc));
 
-
-
-  //free M and L
+  free(Mlm1.data);
+  free(Ml.data);
+  free(Mc.data);
+  free(rx.data);
+  free(ry.data);
+  free(coords.data);
+  free(conn.data);
+  free(phix.data);
+  free(phiy.data);
+  free(u.data);
 
   return 0;
 };
